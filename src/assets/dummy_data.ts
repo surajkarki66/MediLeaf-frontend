2575040a3594e2ee6e9bb51a58b42a8b26d12d27
surajkarki66 <suraj.karki500@protonmail.com>
2023-06-09 18:00:26 +0545

    refactor: modified assets

---
+++ src/assets/dummy_data.ts
@@ -0,0 +1,101 @@
+export const plantsPredictionList = [
+  {
+    scientificName: 'Santalum Album',
+    commonNames: ['White Sandalwood', 'White Sandal'],
+    familyName: 'Sandle wood',
+    confidence: 80,
+    images: [
+      {
+        image:
+          'https://res.cloudinary.com/deek0shwx/image/upload/v1/media/plants/santalum-album/santalum-album_other__wkk8er',
+        part: 'leaf',
+      },
+      {
+        image:
+          'https://res.cloudinary.com/deek0shwx/image/upload/v1/media/plants/santalum-album/santalum-album_other__wkk8er',
+        part: 'leaf',
+      },
+      {
+        image:
+          'https://res.cloudinary.com/deek0shwx/image/upload/v1/media/plants/santalum-album/santalum-album_other__wkk8er',
+        part: 'leaf',
+      },
+      {
+        image:
+          'https://res.cloudinary.com/deek0shwx/image/upload/v1/media/plants/santalum-album/santalum-album_other__wkk8er',
+        part: 'leaf',
+      },
+      {
+        image:
+          'https://res.cloudinary.com/deek0shwx/image/upload/v1/media/plants/santalum-album/santalum-album_other__wkk8er',
+        part: 'leaf',
+      },
+    ],
+  },
+  {
+    scientificName: 'Santalum Album',
+    commonNames: ['White Sandalwood'],
+    familyName: 'Sandle wood',
+    confidence: 80,
+    images: [
+      {
+        image:
+          'https://res.cloudinary.com/deek0shwx/image/upload/v1/media/plants/santalum-album/santalum-album_other__wkk8er',
+        part: 'leaf',
+      },
+      {
+        image:
+          'https://res.cloudinary.com/deek0shwx/image/upload/v1/media/plants/santalum-album/santalum-album_other__wkk8er',
+        part: 'leaf',
+      },
+      {
+        image:
+          'https://res.cloudinary.com/deek0shwx/image/upload/v1/media/plants/santalum-album/santalum-album_other__wkk8er',
+        part: 'leaf',
+      },
+      {
+        image:
+          'https://res.cloudinary.com/deek0shwx/image/upload/v1/media/plants/santalum-album/santalum-album_other__wkk8er',
+        part: 'leaf',
+      },
+      {
+        image:
+          'https://res.cloudinary.com/deek0shwx/image/upload/v1/media/plants/santalum-album/santalum-album_other__wkk8er',
+        part: 'leaf',
+      },
+    ],
+  },
+  {
+    scientificName: 'Santalum Album',
+    commonNames: ['White Sandalwood'],
+    familyName: 'Sandle wood',
+    confidence: 80,
+    images: [
+      {
+        image:
+          'https://res.cloudinary.com/deek0shwx/image/upload/v1/media/plants/santalum-album/santalum-album_other__wkk8er',
+        part: 'leaf',
+      },
+      {
+        image:
+          'https://res.cloudinary.com/deek0shwx/image/upload/v1/media/plants/santalum-album/santalum-album_other__wkk8er',
+        part: 'leaf',
+      },
+      {
+        image:
+          'https://res.cloudinary.com/deek0shwx/image/upload/v1/media/plants/santalum-album/santalum-album_other__wkk8er',
+        part: 'leaf',
+      },
+      {
+        image:
+          'https://res.cloudinary.com/deek0shwx/image/upload/v1/media/plants/santalum-album/santalum-album_other__wkk8er',
+        part: 'leaf',
+      },
+      {
+        image:
+          'https://res.cloudinary.com/deek0shwx/image/upload/v1/media/plants/santalum-album/santalum-album_other__wkk8er',
+        part: 'leaf',
+      },
+    ],
+  },
+];
