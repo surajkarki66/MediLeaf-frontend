721e794e94dcf8bce33c2fd3f024a4d3151774b3
surajkarki66 <suraj.karki500@protonmail.com>
2023-06-25 01:13:49 +0545

    refactor: make unprotected

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 
 import { useFormik } from 'formik';
 import { Loader2 } from 'lucide-react';
@@ -9,8 +9,8 @@ import { LoginType } from '@/@types';
 import AuthLayout from '@/components/layout/AuthLayout';
 import { Button } from '@/components/ui/button';
 import { useToast } from '@/components/ui/use-toast';
+import { AuthContext } from '@/context/AuthContext';
 import { login } from '@/hooks/api';
-import useIsLoggedIn from '@/hooks/useIsLoggedIn';
 import { loginSchema } from '@/schemas/index';
 
 export default function Login() {
@@ -21,6 +21,7 @@ export default function Login() {
   const { toast } = useToast();
   const router = useRouter();
   const [loading, setLoading] = useState(false);
+  const { loginStatus, loading: Loading, getAuth } = useContext(AuthContext);
 
   const { values, handleBlur, handleChange, handleSubmit, errors, touched } =
     useFormik({
@@ -31,7 +32,8 @@ export default function Login() {
       onSubmit: (v, action) => {
         setLoading(true);
         login(v)
-          .then((res) => {
+          .then(async (res) => {
+            await getAuth();
             setLoading(false);
             action.setSubmitting(false);
             action.resetForm();
@@ -56,83 +58,88 @@ export default function Login() {
       },
     });
 
-  const { isLoggedIn } = useIsLoggedIn();
-  if (isLoggedIn) {
-    // return router.push('/');
+  if (loginStatus?.isLoggedIn) {
+    return router.push('/');
   }
+  const getLoginForm = () => {
+    if (Loading) {
+      return null;
+    }
+    return (
+      <AuthLayout type='Login'>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit(e);
+          }}
+        >
+          <div className='mt-5'>
+            <label htmlFor='email'>Email</label>
+            <input
+              type='email'
+              placeholder='Enter an email address'
+              className='border border-gray-400 py-1 px-2 w-full'
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              name='email'
+            />
+            {touched.email && errors.email ? (
+              <p className='text-1.4 text-red-600'>{errors.email}</p>
+            ) : null}
+          </div>
+          <div className='mt-2'>
+            <label htmlFor='password'>Password</label>
+            <input
+              type='password'
+              placeholder='Enter a password'
+              className='border border-gray-400 py-1 px-2 w-full'
+              value={values.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              name='password'
+            />
+            {errors.password && touched.password ? (
+              <p className='text-1.4 text-red-600'>{errors.password}</p>
+            ) : null}
+          </div>
 
-  return (
-    <AuthLayout type='Login'>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          handleSubmit(e);
-        }}
-      >
-        <div className='mt-5'>
-          <label htmlFor='email'>Email</label>
-          <input
-            type='email'
-            placeholder='Enter an email address'
-            className='border border-gray-400 py-1 px-2 w-full'
-            value={values.email}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            name='email'
-          />
-          {touched.email && errors.email ? (
-            <p className='text-1.4 text-red-600'>{errors.email}</p>
-          ) : null}
-        </div>
-        <div className='mt-2'>
-          <label htmlFor='password'>Password</label>
-          <input
-            type='password'
-            placeholder='Enter a password'
-            className='border border-gray-400 py-1 px-2 w-full'
-            value={values.password}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            name='password'
-          />
-          {errors.password && touched.password ? (
-            <p className='text-1.4 text-red-600'>{errors.password}</p>
-          ) : null}
-        </div>
+          <div className='mt-5 mb-5'>
+            <Button
+              variant='default'
+              disabled={loading}
+              className='w-full bg-white border border-[#1E9C5D] py-2 text-center text-md text-[#1E9C5D] hover:text-white hover:bg-[#1E9C5D]'
+              type='submit'
+            >
+              {loading ? (
+                <>
+                  <Loader2 className='mr-2 h-4 w-4 animate-spin' />
+                  Please wait
+                </>
+              ) : (
+                'Login'
+              )}
+            </Button>
+          </div>
+          <div className='flex'>
+            <Link
+              href='/register'
+              className='text-[#1E9C5D] font-semibold font-semibold flex-grow '
+            >
+              Sign up
+            </Link>
 
-        <div className='mt-5 mb-5'>
-          <Button
-            variant='default'
-            disabled={loading}
-            className='w-full bg-white border border-[#1E9C5D] py-2 text-center text-md text-[#1E9C5D] hover:text-white hover:bg-[#1E9C5D]'
-            type='submit'
-          >
-            {loading ? (
-              <>
-                <Loader2 className='mr-2 h-4 w-4 animate-spin' />
-                Please wait
-              </>
-            ) : (
-              'Login'
-            )}
-          </Button>
-        </div>
-        <div className='flex'>
-          <Link
-            href='/register'
-            className='text-[#1E9C5D] font-semibold font-semibold flex-grow '
-          >
-            Sign up
-          </Link>
+            <Link
+              href='/forgot-password'
+              className='text-[#1E9C5D] font-semibold font-semibold '
+            >
+              Forgot Password?
+            </Link>
+          </div>
+        </form>
+      </AuthLayout>
+    );
+  };
 
-          <Link
-            href='/forgot-password'
-            className='text-[#1E9C5D] font-semibold font-semibold '
-          >
-            Forgot Password?
-          </Link>
-        </div>
-      </form>
-    </AuthLayout>
-  );
+  return getLoginForm();
 }
--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 
 import { useFormik } from 'formik';
 import { parsePhoneNumberFromString } from 'libphonenumber-js';
@@ -11,6 +11,7 @@ import { RegisterType } from '@/@types';
 import AuthLayout from '@/components/layout/AuthLayout';
 import { Button } from '@/components/ui/button';
 import { useToast } from '@/components/ui/use-toast';
+import { AuthContext } from '@/context/AuthContext';
 import { signup } from '@/hooks/api';
 import { registerSchema } from '@/schemas/index';
 
@@ -25,6 +26,7 @@ export default function Register() {
     confirm_password: '',
   };
   const router = useRouter();
+  const { loginStatus, loading: Loading } = useContext(AuthContext);
   const [loading, setLoading] = useState(false);
   const [termsAccepted, setTermsAccepted] = useState(false);
   const [termsAcceptedError, setTermsAcceptedError] = useState('');
@@ -68,196 +70,206 @@ export default function Register() {
         }
       },
     });
-  return (
-    <AuthLayout type='Register'>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          if (!termsAccepted) {
-            setTermsAcceptedError('You must accept the terms and conditions');
-          }
-          handleSubmit(e);
-        }}
-      >
-        <div className='mt-4'>
-          <label htmlFor='first_name'>First Name</label>
-          <input
-            type='text'
-            placeholder='Enter the first name'
-            className='border border-gray-400 py-1 px-2 w-full'
-            value={values.first_name}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            name='first_name'
-          />
-          {touched.first_name && errors.first_name ? (
-            <p className='text-1.4 text-red-600'>{errors.first_name}</p>
-          ) : null}
-        </div>
-        <div className='mt-2'>
-          <label htmlFor='last_name'>Last Name</label>
-          <input
-            type='text'
-            placeholder='Enter the last name'
-            className='border border-gray-400 py-1 px-2 w-full'
-            value={values.last_name}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            name='last_name'
-          />
-          {touched.last_name && errors.last_name ? (
-            <p className='text-1.4 text-red-600'>{errors.last_name}</p>
-          ) : null}
-        </div>
-        <div className='mt-2'>
-          <label htmlFor='email'>Email</label>
-          <input
-            type='email'
-            placeholder='Enter an email address'
-            className='border border-gray-400 py-1 px-2 w-full'
-            value={values.email}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            name='email'
-          />
-          {touched.email && errors.email ? (
-            <p className='text-1.4 text-red-600'>{errors.email}</p>
-          ) : null}
-        </div>
-        <div className='mt-2'>
-          <label htmlFor='country'>Country</label>
-          <CountryDropdown
-            classes='country-dropdown'
-            value={values.country ? values.country : ''}
-            onChange={(val, e) => {
-              sessionStorage.setItem('countryCode', val);
-              handleChange(e);
-            }}
-            onBlur={handleBlur}
-            name='country'
-            showDefaultOption
-            valueType='short'
-          />
-          {touched.country && errors.country ? (
-            <p className='text-1.4 text-red-600'>{errors.country}</p>
-          ) : null}
-        </div>
-        <div className='mt-2'>
-          <label htmlFor='contact'>Mobile No.</label>
-          <input
-            type='tel'
-            disabled={values.country === undefined && true}
-            placeholder='Enter a mobile number'
-            className='border border-gray-400 py-1 px-2 w-full'
-            value={values.contact}
-            onChange={(event) => {
-              const input = event.target.value;
-              const numericValue = input.replace(/[^0-9+*#]/g, '');
-              if (numericValue === input) {
-                handleChange(event);
-              }
-            }}
-            onBlur={handleBlur}
-            name='contact'
-          />
-          {touched.contact && errors.contact ? (
-            <p className='text-1.4 text-red-600'>{errors.contact}</p>
-          ) : null}
-        </div>
-        <div className='mt-2'>
-          <label htmlFor='password'>Password</label>
-          <input
-            type='password'
-            placeholder='Enter a password'
-            className='border border-gray-400 py-1 px-2 w-full'
-            value={values.password}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            name='password'
-          />
-          {errors.password && touched.password ? (
-            <p className='text-1.4 text-red-600'>{errors.password}</p>
-          ) : null}
-        </div>
-        <div className='mt-2'>
-          <label htmlFor='confirm_password'>Confirm Password</label>
-          <input
-            type='password'
-            placeholder='Enter confirm password'
-            className='border border-gray-400 py-1 px-2 w-full'
-            value={values.confirm_password}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            name='confirm_password'
-          />
-          {errors.confirm_password && touched.confirm_password ? (
-            <p className='text-1.4 text-red-600'>{errors.confirm_password}</p>
-          ) : null}
-        </div>
-        <div className='mt-5'>
-          <input
-            type='checkbox'
-            className='border border-gray-400'
-            checked={termsAccepted}
-            onChange={(e) => {
-              if (!termsAccepted && termsAcceptedError !== '') {
-                setTermsAcceptedError('');
-              }
-              if (termsAccepted && termsAcceptedError !== '') {
-                setTermsAcceptedError('');
-              }
-              setTermsAccepted(e.target.checked);
-            }}
-            name='termsAccepted'
-          />
-          &nbsp;
-          <span>
-            I accept the{' '}
-            <Link href='#' className='text-[#1E9C5D] font-semibold'>
-              Terms of Use
-            </Link>{' '}
-            &{' '}
-            <Link
-              href='#'
-              className='text-[#1E9C5D] font-semibold font-semibold'
+  if (loginStatus?.isLoggedIn) {
+    return router.push('/');
+  }
+
+  const getRegistrationForm = () => {
+    if (Loading) {
+      return null;
+    }
+    return (
+      <AuthLayout type='Register'>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            if (!termsAccepted) {
+              setTermsAcceptedError('You must accept the terms and conditions');
+            }
+            handleSubmit(e);
+          }}
+        >
+          <div className='mt-4'>
+            <label htmlFor='first_name'>First Name</label>
+            <input
+              type='text'
+              placeholder='Enter the first name'
+              className='border border-gray-400 py-1 px-2 w-full'
+              value={values.first_name}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              name='first_name'
+            />
+            {touched.first_name && errors.first_name ? (
+              <p className='text-1.4 text-red-600'>{errors.first_name}</p>
+            ) : null}
+          </div>
+          <div className='mt-2'>
+            <label htmlFor='last_name'>Last Name</label>
+            <input
+              type='text'
+              placeholder='Enter the last name'
+              className='border border-gray-400 py-1 px-2 w-full'
+              value={values.last_name}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              name='last_name'
+            />
+            {touched.last_name && errors.last_name ? (
+              <p className='text-1.4 text-red-600'>{errors.last_name}</p>
+            ) : null}
+          </div>
+          <div className='mt-2'>
+            <label htmlFor='email'>Email</label>
+            <input
+              type='email'
+              placeholder='Enter an email address'
+              className='border border-gray-400 py-1 px-2 w-full'
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              name='email'
+            />
+            {touched.email && errors.email ? (
+              <p className='text-1.4 text-red-600'>{errors.email}</p>
+            ) : null}
+          </div>
+          <div className='mt-2'>
+            <label htmlFor='country'>Country</label>
+            <CountryDropdown
+              classes='country-dropdown'
+              value={values.country ? values.country : ''}
+              onChange={(val, e) => {
+                sessionStorage.setItem('countryCode', val);
+                handleChange(e);
+              }}
+              onBlur={handleBlur}
+              name='country'
+              showDefaultOption
+              valueType='short'
+            />
+            {touched.country && errors.country ? (
+              <p className='text-1.4 text-red-600'>{errors.country}</p>
+            ) : null}
+          </div>
+          <div className='mt-2'>
+            <label htmlFor='contact'>Mobile No.</label>
+            <input
+              type='tel'
+              disabled={values.country === undefined && true}
+              placeholder='Enter a mobile number'
+              className='border border-gray-400 py-1 px-2 w-full'
+              value={values.contact}
+              onChange={(event) => {
+                const input = event.target.value;
+                const numericValue = input.replace(/[^0-9+*#]/g, '');
+                if (numericValue === input) {
+                  handleChange(event);
+                }
+              }}
+              onBlur={handleBlur}
+              name='contact'
+            />
+            {touched.contact && errors.contact ? (
+              <p className='text-1.4 text-red-600'>{errors.contact}</p>
+            ) : null}
+          </div>
+          <div className='mt-2'>
+            <label htmlFor='password'>Password</label>
+            <input
+              type='password'
+              placeholder='Enter a password'
+              className='border border-gray-400 py-1 px-2 w-full'
+              value={values.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              name='password'
+            />
+            {errors.password && touched.password ? (
+              <p className='text-1.4 text-red-600'>{errors.password}</p>
+            ) : null}
+          </div>
+          <div className='mt-2'>
+            <label htmlFor='confirm_password'>Confirm Password</label>
+            <input
+              type='password'
+              placeholder='Enter confirm password'
+              className='border border-gray-400 py-1 px-2 w-full'
+              value={values.confirm_password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              name='confirm_password'
+            />
+            {errors.confirm_password && touched.confirm_password ? (
+              <p className='text-1.4 text-red-600'>{errors.confirm_password}</p>
+            ) : null}
+          </div>
+          <div className='mt-5'>
+            <input
+              type='checkbox'
+              className='border border-gray-400'
+              checked={termsAccepted}
+              onChange={(e) => {
+                if (!termsAccepted && termsAcceptedError !== '') {
+                  setTermsAcceptedError('');
+                }
+                if (termsAccepted && termsAcceptedError !== '') {
+                  setTermsAcceptedError('');
+                }
+                setTermsAccepted(e.target.checked);
+              }}
+              name='termsAccepted'
+            />
+            &nbsp;
+            <span>
+              I accept the{' '}
+              <Link href='#' className='text-[#1E9C5D] font-semibold'>
+                Terms of Use
+              </Link>{' '}
+              &{' '}
+              <Link
+                href='#'
+                className='text-[#1E9C5D] font-semibold font-semibold'
+              >
+                Privacy Policy
+              </Link>
+            </span>
+            <p className='text-1.4 text-red-600'>{termsAcceptedError}</p>
+          </div>
+          <div className='mt-5 mb-5'>
+            <Button
+              variant='default'
+              disabled={loading}
+              className='w-full bg-white border border-[#1E9C5D] py-2 text-center text-md text-[#1E9C5D] hover:text-white hover:bg-[#1E9C5D]'
+              type='submit'
             >
-              Privacy Policy
+              {loading ? (
+                <>
+                  <Loader2 className='mr-2 h-4 w-4 animate-spin' />
+                  Please wait
+                </>
+              ) : (
+                'Register Now'
+              )}
+            </Button>
+          </div>
+          <div className='flex'>
+            <Link
+              href='/login'
+              className='text-[#1E9C5D] font-semibold font-semibold flex-grow '
+            >
+              Log in
             </Link>
-          </span>
-          <p className='text-1.4 text-red-600'>{termsAcceptedError}</p>
-        </div>
-        <div className='mt-5 mb-5'>
-          <Button
-            variant='default'
-            disabled={loading}
-            className='w-full bg-white border border-[#1E9C5D] py-2 text-center text-md text-[#1E9C5D] hover:text-white hover:bg-[#1E9C5D]'
-            type='submit'
-          >
-            {loading ? (
-              <>
-                <Loader2 className='mr-2 h-4 w-4 animate-spin' />
-                Please wait
-              </>
-            ) : (
-              'Register Now'
-            )}
-          </Button>
-        </div>
-        <div className='flex'>
-          <Link
-            href='/login'
-            className='text-[#1E9C5D] font-semibold font-semibold flex-grow '
-          >
-            Log in
-          </Link>
-          <Link
-            href='/forgot-password'
-            className='text-[#1E9C5D] font-semibold font-semibold '
-          >
-            Forgot Password?
-          </Link>
-        </div>
-      </form>
-    </AuthLayout>
-  );
+            <Link
+              href='/forgot-password'
+              className='text-[#1E9C5D] font-semibold font-semibold '
+            >
+              Forgot Password?
+            </Link>
+          </div>
+        </form>
+      </AuthLayout>
+    );
+  };
+  return getRegistrationForm();
 }
