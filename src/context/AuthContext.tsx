c5eda3b352ae9dc7793a7621aa7d82486b34bb6c
surajkarki66 <suraj.karki500@protonmail.com>
2023-06-25 01:15:49 +0545

    refactor: store loggedInStatus in context api

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,33 +1,72 @@
-import React, { createContext, useState, ReactNode, useEffect } from 'react';
+import React, {
+  createContext,
+  useState,
+  ReactNode,
+  useEffect,
+  useCallback,
+} from 'react';
 
-import { getCsrfToken } from '@/hooks/api';
+import { getCsrfToken, getLoginStatus } from '@/hooks/api';
 
-interface AuthContextProps {
+type AuthContextType = {
   csrf: string | null;
-}
+  setCsrf: React.Dispatch<React.SetStateAction<string | null>>;
+  loginStatus: LoginStatusType | undefined;
+  setLoginStatus: React.Dispatch<
+    React.SetStateAction<LoginStatusType | undefined>
+  >;
+  loading: boolean;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+  getAuth: () => Promise<void>;
+};
 
-export const AuthContext = createContext<AuthContextProps>({
-  csrf: null,
-});
+type LoginStatusType = {
+  isLoggedIn: boolean;
+  data?: { status: string; fullName: string; avatar: string };
+};
+
+export const AuthContext = createContext({} as AuthContextType);
 
 export function AuthProvider({ children }: { children: ReactNode }) {
   const [csrf, setCsrf] = useState<string | null>(null);
+  const [loginStatus, setLoginStatus] = useState<LoginStatusType>();
+  const [loading, setLoading] = useState<boolean>(true);
 
-  useEffect(() => {
-    async function get() {
+  const getAuth = useCallback(async () => {
+    try {
       const { csrfToken } = await getCsrfToken();
       localStorage.setItem('csrfToken', csrfToken);
       setCsrf(csrfToken);
+
+      const data = await getLoginStatus();
+      if (data !== 'undefined' && data?.status === 'success') {
+        setLoginStatus({ isLoggedIn: true, data });
+      } else {
+        setLoginStatus({ isLoggedIn: false });
+      }
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setCsrf(null);
+      setLoginStatus(undefined);
     }
-    get();
   }, []);
+
+  useEffect(() => {
+    getAuth();
+  }, [getAuth]);
 
   const memoedValue = React.useMemo(
     () => ({
       csrf,
       setCsrf,
+      loginStatus,
+      setLoginStatus,
+      loading,
+      setLoading,
+      getAuth,
     }),
-    [csrf]
+    [csrf, loading, loginStatus, getAuth]
   );
 
   return (
